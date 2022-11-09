7b282444aa84e4110ae56096c8419a9d89f1156c
ATomShermental <nike123a44@gmail.com>
2022-11-09 18:40:24 +0300

    initial commit

---
+++ Spring-Boot-News-Service/src/main/java/com/atomshermental/springbootnewsservice/SpringBootNewsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atomshermental.springbootnewsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNewsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNewsServiceApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-News-Service/src/test/java/com/atomshermental/springbootnewsservice/SpringBootNewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomshermental.springbootnewsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
